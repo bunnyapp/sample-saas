8c4aeb4526c20f5a8ddc4bbf88ac414c1d36d267
richet <richardchetwynd@gmail.com>
2022-11-02 18:24:35 +0200

    added error logging around db create

--- db/index.js
+++ db/index.js
@@ -6,34 +6,38 @@ const query = (text, params) => pool.query(text, params);
 (async () => {
   console.log("Initializing DB");
 
-  await pool.query(
-    "CREATE TABLE IF NOT EXISTS users ( \
-    id SERIAL PRIMARY KEY, \
-    first_name TEXT, \
-    last_name TEXT, \
-    email TEXT UNIQUE, \
-    max_notes INTEGER, \
-    bunny_code TEXT UNIQUE \
-  )"
-  );
+  try {
+    await pool.query(
+      "CREATE TABLE IF NOT EXISTS users ( \
+      id SERIAL PRIMARY KEY, \
+      first_name TEXT, \
+      last_name TEXT, \
+      email TEXT UNIQUE, \
+      max_notes INTEGER, \
+      bunny_code TEXT UNIQUE \
+    )"
+    );
 
-  await pool.query(
-    "CREATE TABLE IF NOT EXISTS notes ( \
-    id SERIAL PRIMARY KEY, \
-    user_id INTEGER NOT NULL, \
-    note TEXT NOT NULL, \
-    completed INTEGER \
-  )"
-  );
+    await pool.query(
+      "CREATE TABLE IF NOT EXISTS notes ( \
+      id SERIAL PRIMARY KEY, \
+      user_id INTEGER NOT NULL, \
+      note TEXT NOT NULL, \
+      completed INTEGER \
+    )"
+    );
 
-  await pool.query(
-    "CREATE TABLE IF NOT EXISTS events ( \
-    id SERIAL PRIMARY KEY, \
-    user_id INTEGER NOT NULL, \
-    message TEXT NOT NULL, \
-    created_at TIMESTAMP DEFAULT NOW()\
-  )"
-  );
+    await pool.query(
+      "CREATE TABLE IF NOT EXISTS events ( \
+      id SERIAL PRIMARY KEY, \
+      user_id INTEGER NOT NULL, \
+      message TEXT NOT NULL, \
+      created_at TIMESTAMP DEFAULT NOW()\
+    )"
+    );
+  } catch (error) {
+    console.log("Error creating database", error);
+  }
 
   console.log("Initializing DB Complete");
 })();
