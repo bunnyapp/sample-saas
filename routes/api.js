b6bedd0030a1139d31ff93942dca2d59dc3c9629
richet <richardchetwynd@gmail.com>
2024-03-04 08:09:10 -0400

    added checkout validation

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var crypto = require("crypto");
 
+var db = require("../db");
 var accountsService = require("../services/accounts");
 var eventsService = require("../services/events");
 
@@ -35,6 +36,107 @@ function getNotesAllowedFromSubcriptions(subscriptions) {
   return notesAllowed?.quantity || 3;
 }
 
+function getNotesAllowedFromFeatures(features) {
+  const notesAllowed = features.find((f) => f.code == "notes");
+  return notesAllowed?.quantity || 3;
+}
+
+async function processCheckoutValidation(payload) {
+  console.log("Processing checkout validation", payload);
+
+  var accountId = payload.tenant.code.replace("sample-saas-account-", "");
+  var account = await accountsService.findById(accountId);
+  console.log("ACCOUNT", account);
+
+  const maxNotes = getNotesAllowedFromFeatures(payload.change.features);
+
+  // If the account has created more notes than the notes quantity in the validation webhook
+  // payload then we cant allow the change to happen so we return an invalid response
+  const { rows } = await db.query(
+    "SELECT COUNT(*) FROM notes WHERE user_id = $1",
+    [account.id]
+  );
+  const totalNotes = rows[0].count;
+
+  return totalNotes < maxNotes;
+}
+
+async function processProvisioningChange(payload) {
+  console.log("Processing provisioning change", payload);
+
+  var accountId = payload.tenant.code.replace("sample-saas-account-", "");
+  var account = await accountsService.findById(accountId);
+  console.log("ACCOUNT", account);
+
+  const contact = payload.tenant.account.contacts[0];
+  const subscription = payload.change.subscriptions[0];
+
+  await eventsService.createEvent(
+    0,
+    "Provisioning request received from Bunny for tenant " + payload.tenant.code
+  );
+
+  const maxNotes = getNotesAllowedFromSubcriptions(
+    payload.change.subscriptions
+  );
+
+  if (account) {
+    await eventsService.createEvent(
+      account.id,
+      "Subscription state: " + subscription.state
+    );
+
+    await eventsService.createEvent(account.id, "Updating account features");
+
+    await eventsService.createEvent(
+      account.id,
+      "Maximum notes set to " + maxNotes.toString()
+    );
+
+    // Update the account
+    const updateResponse = await accountsService.updateMaxNotes(
+      account.id,
+      maxNotes
+    );
+
+    return res.json({ success: updateResponse });
+  } else {
+    // Create a new account
+    var account = await accountsService.createAccount(
+      contact.first_name,
+      contact.last_name,
+      contact.email,
+      maxNotes
+    );
+
+    await eventsService.createEvent(
+      account.id,
+      "Account created via Bunny provisioning"
+    );
+
+    await eventsService.createEvent(
+      account.id,
+      "Maximum notes set to " + maxNotes.toString()
+    );
+
+    try {
+      // Then update the tenantCode on Bunny
+      const tenant = await bunny.tenantUpdate(
+        payload.tenant.id,
+        account.id,
+        payload.tenant.name
+      );
+
+      await eventsService.createEvent(
+        account.id,
+        "Updated tenant code in Bunny to " + account.id.toString()
+      );
+    } catch (error) {
+      console.log("Updating Bunny Tenant Failed", error);
+    }
+  }
+}
+
 router.post("/hook", validateWebhookSignature, async function (req, res, next) {
   console.log("Webhook Received", req.body);
   console.log(req.body.type);
@@ -48,84 +150,22 @@ router.post("/hook", validateWebhookSignature, async function (req, res, next) {
       if (payload.tenant.code == "provisioning-test")
         return res.sendStatus(200);
 
-      var accountId = payload.tenant.code.replace("sample-saas-account-", "");
-      var account = await accountsService.findById(accountId);
-      console.log("ACCOUNT", account);
-
-      const contact = payload.tenant.account.contacts[0];
-      const subscription = payload.change.subscriptions[0];
-
-      await eventsService.createEvent(
-        0,
-        "Provisioning request received from Bunny for tenant " +
-          payload.tenant.code
-      );
-
-      const maxNotes = getNotesAllowedFromSubcriptions(
-        payload.change.subscriptions
-      );
-
-      if (account) {
-        await eventsService.createEvent(
-          account.id,
-          "Subscription state: " + subscription.state
-        );
-
-        await eventsService.createEvent(
-          account.id,
-          "Updating account features"
-        );
-
-        await eventsService.createEvent(
-          account.id,
-          "Maximum notes set to " + maxNotes.toString()
-        );
-
-        // Update the account
-        const updateResponse = await accountsService.updateMaxNotes(
-          account.id,
-          maxNotes
-        );
-
-        return res.json({ success: updateResponse });
-      } else {
-        // Create a new account
-        var account = await accountsService.createAccount(
-          contact.first_name,
-          contact.last_name,
-          contact.email,
-          maxNotes
-        );
-
-        await eventsService.createEvent(
-          account.id,
-          "Account created via Bunny provisioning"
-        );
-
-        await eventsService.createEvent(
-          account.id,
-          "Maximum notes set to " + maxNotes.toString()
-        );
-
-        try {
-          // Then update the tenantCode on Bunny
-          const tenant = await bunny.tenantUpdate(
-            payload.tenant.id,
-            account.id,
-            payload.tenant.name
-          );
-
-          await eventsService.createEvent(
-            account.id,
-            "Updated tenant code in Bunny to " + account.id.toString()
-          );
-        } catch (error) {
-          console.log("Updating Bunny Tenant Failed", error);
-        }
-      }
+      await processProvisioningChange(payload);
 
       return res.json({ success: true });
 
+    case "CheckoutValidation":
+      const valid = await processCheckoutValidation(payload);
+
+      if (valid) {
+        return res.sendStatus(200);
+      }
+
+      return res
+        .status(422)
+        .json([
+          "You have too many notes to change to this plan. Delete some notes and try again.",
+        ]);
     default:
       return res.sendStatus(400);
   }
